9863c60159b65ab1fb9062d4c911fa09caa187a2
subash <imsuubash@gmail.com>
2022-07-20 15:36:30 +0545

    navbar improve
    More changes
     - add accent color to root variable
     - implement search bar
     - add alpha value to root

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import { ShoppingCartIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+// import {SearchIcon}
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import { useAppSelector } from "../hooks/reduxHook";
@@ -7,10 +9,22 @@ function Navbar() {
   const products = useAppSelector((state) => state.cart);
   return (
     <nav className="nav">
-      <Link to="/">CRC</Link>
+      <Link to="/">
+        <p className="logo">CRC</p>
+      </Link>
       <div className="links">
-        <Link to="/">Home</Link>
-        {/* <Link to="/cart">Cart</Link> */}
+        <div className="search-with-icon flex flex-grow items-center w-full h-full bg-red-200 rounded-md">
+          <div className="input-cotainer bg-white flex items-center rounded-r-md flex-grow">
+            <input
+              className="input-search p-3 outline-none flex-grow rounded-r-md"
+              placeholder="search.."
+              type="search"
+            />
+          </div>
+          <div className="input-icon bg-purple-600 text-white py-3 px-2 rounded-l-md flex items-center justify-center">
+            <SearchIcon className="search-icon w-6 h-6 " />
+          </div>
+        </div>
       </div>
       <Link to="/cart">
         <div className="icon-container shopping-cart">
