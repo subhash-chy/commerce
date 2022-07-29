5aa07c6aaf697164f70606c3d03e0f5069b144bd
subash <imsuubash@gmail.com>
2022-07-29 15:10:30 +0545

    extract ads to separate folder

---
+++ src/constants/ads.ts
@@ -0,0 +1,37 @@
+interface AdType {
+  img: string;
+  text: string;
+  desc?: string;
+  link?: string;
+}
+
+export const ads: AdType[] = [
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/828b66b4-f4f0-4930-ae11-6e66565b1e1d.jpg_1200x1200.jpg",
+    text: "Ad 1",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/454b692e-4d1d-4ded-8a57-92001d62fb61.jpg",
+    text: "Ad 2",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/1f0684f8-b8ba-4161-8348-0b2e35c576f0.jpg",
+    text: "Ad 3",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/a2f6ac10-1c40-4824-9a7f-609dff95493a.jpg",
+    text: "Ad 4",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/54c5ac85-4269-49f5-a99e-0ff3a91091f1.jpg",
+    text: "Ad 5",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/cb2ec7e0-9fbd-46e7-8265-6607f2c4a07c.png",
+    text: "Ad 6",
+  },
+  {
+    img: "https://icms-image.slatic.net/images/ims-web/e8caf407-8c83-4056-bd93-800378e11f51.jpg",
+    text: "Ad 7",
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,11 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "../styles/Swiper.css";
 import SwiperCore, { Autoplay, Pagination } from "swiper";
+import { ads } from "../constants/ads";
 
 SwiperCore.use([Autoplay]);
 
 function Home() {
-  const ads = [
-    "https://icms-image.slatic.net/images/ims-web/828b66b4-f4f0-4930-ae11-6e66565b1e1d.jpg_1200x1200.jpg",
-    "https://icms-image.slatic.net/images/ims-web/454b692e-4d1d-4ded-8a57-92001d62fb61.jpg",
-    "https://icms-image.slatic.net/images/ims-web/1f0684f8-b8ba-4161-8348-0b2e35c576f0.jpg",
-    "https://icms-image.slatic.net/images/ims-web/a2f6ac10-1c40-4824-9a7f-609dff95493a.jpg",
-    "https://icms-image.slatic.net/images/ims-web/54c5ac85-4269-49f5-a99e-0ff3a91091f1.jpg",
-    "https://icms-image.slatic.net/images/ims-web/cb2ec7e0-9fbd-46e7-8265-6607f2c4a07c.png",
-    "https://icms-image.slatic.net/images/ims-web/e8caf407-8c83-4056-bd93-800378e11f51.jpg",
-  ];
-
   return (
     <>
       {/* Swiper */}
@@ -34,7 +25,11 @@ function Home() {
           {ads.map((ad, id) => (
             <SwiperSlide key={id}>
               <div className="slides">
-                <img className="slide-image" src={ad} alt={`ad ${id + 1}`} />
+                <img
+                  className="slide-image"
+                  src={ad.img}
+                  alt={`ad ${id + 1}`}
+                />
               </div>
             </SwiperSlide>
           ))}
