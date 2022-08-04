9d6feaa59e73410986ca238fa9b1cec008f25781
subash <imsuubash@gmail.com>
2022-08-04 23:12:46 +0545

    declare types for khalti module

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,8 +3,12 @@ declare module "*.module.css";
 interface ImportMetaEnv {
   readonly VITE_AUTH0_DOMAIN: string;
   readonly VITE_AUTH0_CLIENT_ID: string;
+  readonly VITE_KHALTI_PUBLIC_KEY: string;
+  readonly KHALTI_SECRET_KEY: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module "khalti-checkout-web";
